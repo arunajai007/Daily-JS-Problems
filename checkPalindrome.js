2894d6f1b6098590ff82eb598af77f055a60843a
arunajai007 <arunvijay0086@gmail.com>
2026-02-09 17:25:27 +0530

    Check palindrome or not

---
+++ checkPalindrome.js
@@ -0,0 +1,12 @@
+function isPalindrome(str){
+    str = str.toLowerCase();
+
+    for(let i =0; i<str.length/2; i++){
+        if(str[i] !== str[str.length-1-i]){
+            return false
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome("RaceCar"))
