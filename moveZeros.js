711f46d77d291153fe0734ec205cc7833ced2e2f
arunajai007 <arunvijay0086@gmail.com>
2026-02-25 22:18:11 +0530

    Move all the zeros to the last of an array

---
+++ moveZeros.js
@@ -0,0 +1,17 @@
+function moveZeros(arr){
+    let index = 0;
+
+    for(let i =0; i<arr.length; i++){
+        if(arr[i] !== 0){
+            arr[index] = arr[i]
+            index++
+        }
+    }
+    while(index < arr.length){
+        arr[index] =0;
+        index++
+    }
+    return arr
+}
+
+console.log(moveZeros([1,0,4,0,5]))
